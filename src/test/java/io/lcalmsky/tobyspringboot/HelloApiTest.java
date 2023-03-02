639dbf354755bd72c890cfc004e83bb7e9ce0335
Jaime <lcalmsky@gmail.com>
2023-03-03 01:18:04 +0900

    테스트 코드를 이용한 테스트
    (cherry picked from commit 5c599841f9f5a80a99d8ab3bc29f6a910a05c61d)

---
+++ src/test/java/io/lcalmsky/tobyspringboot/HelloApiTest.java
@@ -0,0 +1,32 @@
+package io.lcalmsky.tobyspringboot;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Objects;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+public class HelloApiTest {
+
+  @Test
+  void helloApi() {
+    // given
+    TestRestTemplate testRestTemplate = new TestRestTemplate();
+    // when
+    ResponseEntity<String> responseEntity = testRestTemplate.getForEntity(
+        "http://localhost:8080/hello?name={name}", String.class, "jaime");
+    // then
+    assertSame(HttpStatus.OK, responseEntity.getStatusCode());
+    assertEquals("hello jaime", responseEntity.getBody());
+    assertTrue(
+        Objects.requireNonNull(responseEntity.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE))
+            .startsWith(MediaType.TEXT_PLAIN_VALUE));
+  }
+
+}
