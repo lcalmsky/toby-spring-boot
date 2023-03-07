b3d9860c34db9218f0ba6b6e2cde4cc6742dd468
Jaime <lcalmsky@gmail.com>
2023-03-08 01:30:29 +0900

    커스텀 `@Conditional`

--- src/main/java/io/lcalmsky/config/autoconfig/JettyWebServerConfiguration.java
+++ src/main/java/io/lcalmsky/config/autoconfig/JettyWebServerConfiguration.java
@@ -1,29 +1,17 @@
 package io.lcalmsky.config.autoconfig;
 
+import io.lcalmsky.config.ConditionalMyOnClass;
 import io.lcalmsky.config.MyAutoConfiguration;
-import io.lcalmsky.config.autoconfig.JettyWebServerConfiguration.JettyCondition;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
-@Conditional(JettyCondition.class)
+@ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfiguration {
 
   @Bean("jettyWebServerFactory")
   public ServletWebServerFactory servletWebServerFactory() {
     return new JettyServletWebServerFactory();
-  }
-
-  static class JettyCondition implements Condition {
-
-    @Override
-    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-      return true;
-    }
   }
 }
--- src/main/java/io/lcalmsky/config/autoconfig/TomcatWebServerConfiguration.java
+++ src/main/java/io/lcalmsky/config/autoconfig/TomcatWebServerConfiguration.java
@@ -1,29 +1,17 @@
 package io.lcalmsky.config.autoconfig;
 
-import io.lcalmsky.config.autoconfig.TomcatWebServerConfiguration.TomcatCondition;
+import io.lcalmsky.config.ConditionalMyOnClass;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @Configuration
-@Conditional(TomcatCondition.class)
+@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfiguration {
 
   @Bean("tomcatWebServerFactory")
   public ServletWebServerFactory servletWebServerFactory() {
     return new TomcatServletWebServerFactory();
-  }
-
-  static class TomcatCondition implements Condition {
-
-    @Override
-    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-      return false;
-    }
   }
 }
