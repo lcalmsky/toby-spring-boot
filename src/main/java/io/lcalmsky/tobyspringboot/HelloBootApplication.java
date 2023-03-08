0cca3ead2546013ae904ff8a7597ecf6c0d84f96
Jaime <lcalmsky@gmail.com>
2023-03-09 00:28:42 +0900

    Environment 우선순위 테스트

--- src/main/java/io/lcalmsky/tobyspringboot/HelloBootApplication.java
+++ src/main/java/io/lcalmsky/tobyspringboot/HelloBootApplication.java
@@ -1,10 +1,21 @@
 package io.lcalmsky.tobyspringboot;
 
 import io.lcalmsky.config.MySpringBootApplication;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @MySpringBootApplication
 public class HelloBootApplication {
+
+  @Bean
+  ApplicationRunner applicationRunner(Environment environment) {
+    return args -> {
+      String name = environment.getProperty("my.name");
+      System.out.println("name = " + name);
+    };
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(HelloBootApplication.class, args);
