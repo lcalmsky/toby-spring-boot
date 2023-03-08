a868c1ca6f8bb9c1378da471312fc64d892d1c57
Jaime <lcalmsky@gmail.com>
2023-03-09 00:50:53 +0900

    프로퍼티 클래스의 분리(Binder 사용)

--- src/main/java/io/lcalmsky/config/autoconfig/ServerPropertiesConfiguration.java
+++ src/main/java/io/lcalmsky/config/autoconfig/ServerPropertiesConfiguration.java
@@ -1,6 +1,7 @@
 package io.lcalmsky.config.autoconfig;
 
 import io.lcalmsky.config.MyAutoConfiguration;
+import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
@@ -9,9 +10,6 @@ public class ServerPropertiesConfiguration {
 
   @Bean
   ServerProperties serverProperties(Environment environment) {
-    ServerProperties serverProperties = new ServerProperties();
-    serverProperties.setContextPath(environment.getProperty("context-path"));
-    serverProperties.setPort(Integer.parseInt(environment.getProperty("port")));
-    return serverProperties;
+    return Binder.get(environment).bind("", ServerProperties.class).get();
   }
 }
