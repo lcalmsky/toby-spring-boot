cf9e01aaa5e79f7c58f4133cbf90b01cff978035
Jaime <lcalmsky@gmail.com>
2023-03-10 02:23:45 +0900

    JdbcTemplate과 트랜잭션 매니저 구성

--- src/main/java/io/lcalmsky/config/autoconfig/DataSourceConfiguration.java
+++ src/main/java/io/lcalmsky/config/autoconfig/DataSourceConfiguration.java
@@ -7,12 +7,17 @@ import io.lcalmsky.config.MyAutoConfiguration;
 import java.sql.Driver;
 import javax.sql.DataSource;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
 @EnableMyConfigurationProperties(MyDataSourceProperties.class)
+@EnableTransactionManagement
 public class DataSourceConfiguration {
 
   @Bean
@@ -37,5 +42,19 @@ public class DataSourceConfiguration {
     dataSource.setUsername(properties.getUsername());
     dataSource.setPassword(properties.getPassword());
     return dataSource;
+  }
+
+  @Bean
+  @ConditionalOnSingleCandidate(DataSource.class)
+  @ConditionalOnMissingBean
+  JdbcTemplate jdbcTemplate(DataSource dataSource) {
+    return new JdbcTemplate(dataSource);
+  }
+
+  @Bean
+  @ConditionalOnSingleCandidate(DataSource.class)
+  @ConditionalOnMissingBean
+  JdbcTransactionManager jdbcTransactionManager(DataSource dataSource) {
+    return new JdbcTransactionManager(dataSource);
   }
 }
---
+++ src/test/java/io/lcalmsky/tobyspringboot/JdbcTemplateTest.java
@@ -0,0 +1,29 @@
+package io.lcalmsky.tobyspringboot;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@HellobootTest
+public class JdbcTemplateTest {
+
+  @Autowired
+  JdbcTemplate jdbcTemplate;
+
+  @BeforeEach
+  void setup() {
+    jdbcTemplate.execute(
+        "create table if not exists hello(name varchar(50) primary key, count int)");
+  }
+
+  @Test
+  void insertAndQuery() {
+    jdbcTemplate.update("insert into hello values(?, ?)", "jaime", 3);
+    jdbcTemplate.update("insert into hello values(?, ?)", "spring", 1);
+    Long count = jdbcTemplate.queryForObject("select count(*) from hello", Long.class);
+    assertEquals(2, count);
+  }
+}
